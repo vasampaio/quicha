2bf9ba5c6154b083cc98ba4f26f58b4e99469cc1
Vinicius Sampaio <vinicius.sampaio@aluno.uece.br>
2019-11-22 08:48:06 -0300

    fixed t-sne plot

--- c3/Main_Script.m
+++ c3/Main_Script.m
@@ -2,12 +2,15 @@ pkg load statistics
 x = csvread('new_iris.data');
 X = x(:,1:4);
 y = x(:,5);
+
 [idx, centers, sumd, dist] = kmeans(X, 3);
-mappedX = tsne(X, idx, 2, 4, 30);
+mappedX = tsne(X, [], 2, 4, 30);
+scatter(mappedX(:,1), mappedX(:,2),36, idx);
 title ("K-means Clustering with t-SNE");
-axis("on","auto[xyz]")
-print ( "kcluster.pdf");
-mappedX = tsne(X, y, 2, 4, 30);
+axis("on","auto[xyz]");
+print ("kmeans_cluster.pdf");
+
+scatter(mappedX(:,1), mappedX(:,2),36, y);
 title ("Original Clustering with t-SNE");
-axis("on","auto[xyz]")
-print ( "ocluster.pdf");
+axis("on","auto[xyz]");
+print ("original_cluster.pdf");
